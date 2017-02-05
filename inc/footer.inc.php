97d01389340443283b45016eff1d6e9e16d0c5ba
Crystal <lorenz.rein84@gmail.com>
2017-02-06 06:10:39 +0800

    WIP - updated responsive footer

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -5,33 +5,15 @@
 <div id="footer">
 
 
-
 	<?php echo ShowFooterPages(); ?>
 
 	<a href="<?php echo SITE_URL; ?>aboutus.php"><?php echo CBE1_FMENU_ABOUT; ?></a> &middot; 
 
-	<a href="<?php echo SITE_URL; ?>news.php"><?php echo CBE1_FMENU_NEWS; ?></a> &middot; 
-
 	<a href="<?php echo SITE_URL; ?>terms.php"><?php echo CBE1_FMENU_TERMS; ?></a> &middot; 
 
-	<a href="<?php echo SITE_URL; ?>privacy.php"><?php echo CBE1_FMENU_PRIVACY; ?></a> &middot; 
-
-	<a href="<?php echo SITE_URL; ?>contact.php"><?php echo CBE1_FMENU_CONTACT; ?></a> &middot; 
-
-	<a href="<?php echo SITE_URL; ?>rss.php" class="rss"><?php echo CBE1_FMENU_RSS; ?></a>
-
-	
+	<a href="<?php echo SITE_URL; ?>contact.php"><?php echo CBE1_FMENU_CONTACT; ?></a>
 
 	<p>&copy; 2015 <?php echo SITE_TITLE; ?>. <?php echo CBE1_FMENU_RIGHTS; ?>.</p>
-
-
-
-	<!-- Do not remove this copyright notice! -->
-
-	<div class="powered-by-cashbackengine">Powered by <a href="http://www.cashbackengine.net" title="CashbackEngine - cashback site script" target="_blank"><span style="color: #94D802">Cashback</span><span style="color: #5BADFF">Engine</span></a><div>
-
-	<!-- Do not remove this copyright notice! -->
-
 
 
 </div>
