08dfc26d8dc4d8346db7af4269ee13465f5e3420
Crystal <lorenz.rein84@gmail.com>
2017-02-18 00:08:25 +0800

    WIP - updated url on logout action

--- logout.php
+++ logout.php
@@ -1,21 +1,41 @@
-<?php
-/*******************************************************************\
- * CashbackEngine v3.0
- * http://www.CashbackEngine.net
- *
- * Copyright (c) 2010-2015 CashbackEngine Software. All rights reserved.
- * ------------ CashbackEngine IS NOT FREE SOFTWARE --------------
-\*******************************************************************/
-
-	session_start();
-	
-	unset($_SESSION['userid'], $_SESSION['FirstName'], $_SESSION['goto'], $_SESSION['goto_created'], $_SESSION['goRetailerID'], $_SESSION['goCouponID'], $_SESSION['password_verified']);
-	
-	session_destroy();
-
-	setcookie("usname", "", time()-3600);
-
-	header("Location: login.php");
-	exit();
-	
+<?php
+
+/*******************************************************************\
+
+ * CashbackEngine v3.0
+
+ * http://www.CashbackEngine.net
+
+ *
+
+ * Copyright (c) 2010-2015 CashbackEngine Software. All rights reserved.
+
+ * ------------ CashbackEngine IS NOT FREE SOFTWARE --------------
+
+\*******************************************************************/
+
+
+
+	session_start();
+
+	
+
+	unset($_SESSION['userid'], $_SESSION['FirstName'], $_SESSION['goto'], $_SESSION['goto_created'], $_SESSION['goRetailerID'], $_SESSION['goCouponID'], $_SESSION['password_verified']);
+
+	
+
+	session_destroy();
+
+
+
+	setcookie("usname", "", time()-3600);
+
+
+
+	header("Location: index.php");
+
+	exit();
+
+	
+
 ?>
